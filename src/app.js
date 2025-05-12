0a30d77ed5e3bfbe4d67bc2636059aca381b5bff
Ravi <ravi.raushan.nl@gmail.com>
2025-05-12 10:43:20 +0530

    Created: express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("Starting with DevTinder");
+const express = require("express");
+
+const PORT = 4444;
+const app = express();
+
+// listen the server
+app.listen(4444, () => {
+  console.log("Server is listening on port", PORT);
+});

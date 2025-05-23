19ddabc18ba9cb598e5bd87d48b0f98b517e349b
Ravi <ravi.raushan.nl@gmail.com>
2025-05-24 02:30:05 +0530

    Added: updateUserInfoHandler in handler/index

--- src/handler/index.js
+++ src/handler/index.js
@@ -2,7 +2,7 @@ const { registerUserHandler } = require("./registerUser");
 const { fetchUserHandler } = require("./fetchUser");
 const { fetchUsersHandler } = require("./fetchUsers");
 const { deleteUserHandler } = require("./deleteUser");
-const { updateUserInfoHandler } = require("./updateUser");
+const { updateUserInfoHandler } = require("./updateUserInfo");
 
 module.exports = {
   registerUserHandler,

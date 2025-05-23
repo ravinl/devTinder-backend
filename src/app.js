be4b1e702b0e3b9aab16a22373ed60fcf7fc6dad
Ravi <ravi.raushan.nl@gmail.com>
2025-05-23 14:36:43 +0530

    Listen: Server at PORT 300

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const { connectDB } = require("./dataBase/connection");
 
 const server = express();
-
 const PORT = 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1); // Exit the process with failure
+  });
